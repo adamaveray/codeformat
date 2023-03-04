49b7c61d1c0b3f7a230844f1ee76d6b156b90e71
Adam Averay <adam@averay.com>
2023-03-04 23:12:22 +0900

    Use modern recommended ESLint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
 import typescript from '@typescript-eslint/eslint-plugin';
 import typescriptParser from '@typescript-eslint/parser';
 import prettierConfig from 'eslint-config-prettier';
@@ -8,13 +9,16 @@ import prettierConfig from 'eslint-config-prettier';
 const compat = new FlatCompat({ baseDirectory: path.dirname(fileURLToPath(import.meta.url)) });
 
 export default [
-  'eslint:recommended',
-  { files: ['**/*.{js,jsx,cjs,cjsx,mjs,mjsx}'] },
+  {
+    files: ['**/*.{js,jsx,cjs,cjsx,mjs,mjsx}'],
+    rules: js.configs.recommended.rules,
+  },
   {
     ...compat.plugins('@typescript-eslint')[0],
     files: ['**/*.{ts,tsx,cts,ctsx,mts,mtsx}'],
     languageOptions: { parser: typescriptParser },
     rules: {
+      ...js.configs.recommended.rules,
       ...typescript.configs.base.rules,
       ...typescript.configs['eslint-recommended'].rules,
       ...typescript.configs.strict.rules,

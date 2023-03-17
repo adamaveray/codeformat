8d1cac3f45cbdb19a287f7a6ba6a6fe21a4ce2c9
Adam Averay <adam@averay.com>
2023-03-17 14:56:31 +0900

    Standardise calling formatting utilities

---
+++ bin-codeformat.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+# Parse arguments
+ACTION="$1"
+DIR="$2"
+if [ "$DIR" = '' ]; then
+  DIR='.'
+fi
+
+CONFIG_PATH_TYPESCRIPT="$DIR/tsconfig.json"
+
+if [ "$ACTION" = 'check' ]; then
+  npx eslint "$DIR"
+  npx prettier --check "$DIR"
+  if [ -f "$CONFIG_PATH_TYPESCRIPT" ]; then
+    npx tsc --noEmit "$DIR/tsconfig.json"
+  fi
+  npx stylelint --allow-empty-input "$DIR/**/*.{css,sass,scss}"
+elif [ "$ACTION" = 'fix' ]; then
+  npx eslint --fix "$DIR"
+  npx prettier --write "$DIR"
+  npx stylelint --fix --allow-empty-input "$DIR/**/*.{css,sass,scss}"
+else
+  # Invalid/unset arguments
+  echo "Usage:
+  $0 check [root-path]
+  $0 fix [root-path]" >&2
+  exit 1
+fi

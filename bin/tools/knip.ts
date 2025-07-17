612145a84474ff2c8e6dbf671e76d791711fd0cb
Adam Averay <adam@averay.com>
2025-07-17 11:19:45 +0100

    Adjust Knip arguments
    Strict mode causes too many false-positives.

--- bin/tools/knip.ts
+++ bin/tools/knip.ts
@@ -2,12 +2,13 @@ import { commonExts, withExts } from '../utils/filesystem.ts';
 import runners from '../utils/runners.ts';
 import type { Tool } from '../utils/types.ts';
 
+const commonArgs = ['--treat-config-hints-as-errors', '--isolate-workspaces'];
 export default {
   exec: runners.bun,
   command: 'knip',
   actions: (configPath) => ({
-    check: ['--strict', '--config', configPath],
-    fix: ['--fix', '--strict', '--config', configPath],
+    check: ['--config', configPath, ...commonArgs],
+    fix: ['--fix', '--config', configPath, ...commonArgs],
   }),
   args: {
     debug: ['--debug'],

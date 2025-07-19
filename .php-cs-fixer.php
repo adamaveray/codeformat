306b78fed641623bf7d2e055d72f8b5800a1b655
Adam Averay <adam@averay.com>
2025-07-19 17:07:02 +0100

    Set correct ignore paths for PHP CS Fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/php/PhpCsFixerConfig.php';
 
-$finder = new PhpCsFixer\Finder()->in([__DIR__])->exclude(['var']);
+$finder = new PhpCsFixer\Finder()->in([__DIR__])->exclude(['dist', 'node_modules', 'vendor']);
 
 return PhpCsFixerConfig::default($finder);

c1956724daa3e17ceb01eb24acfc8caa8b97d6f9
Adam Averay <adam@averay.com>
2026-04-04 13:34:11 +0100

    Resolve this-binding warning

--- bin/utils/runners.ts
+++ bin/utils/runners.ts
@@ -1,10 +1,10 @@
 import type { ToolExec } from './types.ts';
 
 export default {
-  async bun(cli, { command, args, env = {} }) {
+  async bun(this: void, cli, { command, args, env = {} }) {
     return cli.runSubprocess(['bun', '--bun', 'x', command, ...args], env);
   },
-  async composer(cli, { command, args, env = {} }) {
+  async composer(this: void, cli, { command, args, env = {} }) {
     return cli.runSubprocess(['composer', 'exec', command, '--', ...args], env);
   },
 } satisfies Record<string, ToolExec>;

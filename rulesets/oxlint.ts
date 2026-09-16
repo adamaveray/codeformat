59f24ccf14648d7ea2f0d784ecfe9cb29857c897
Adam Averay <adam@averay.com>
2026-09-16 15:17:36 +0100

    Configure rule to permit void in generic types

--- rulesets/oxlint.ts
+++ rulesets/oxlint.ts
@@ -288,7 +288,7 @@ export const shared = {
   'typescript/no-empty-object-type': ['error', { allowObjectTypes: 'always' }],
   'typescript/no-extraneous-class': 'off', // Sometimes useful as a pseudo-namespace.
   'typescript/no-inferrable-types': ['error', { ignoreParameters: true }],
-  'typescript/no-invalid-void-type': ['error', { allowAsThisParameter: true }],
+  'typescript/no-invalid-void-type': ['error', { allowAsThisParameter: true, allowInGenericTypeArguments: true }],
   'typescript/no-namespace': ['error', { allowDeclarations: true, allowDefinitionFiles: true }],
   'typescript/no-unnecessary-condition': ['error', { allowConstantLoopConditions: true }],
   'typescript/no-unnecessary-type-assertion': 'off', // Too buggy under oxlint.
